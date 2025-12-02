cc91a13581e33bfa6f654ca5c503b04412ddd5cf
ly4sh <anatolimihaelov53@gmail.com>
2025-12-02 22:09:42 +0200

    TokenDto

---
+++ src/main/java/dto/ContactsDto.java
@@ -0,0 +1,15 @@
+package dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContactsDto {
+    private List<Contact> contacts;
+}
